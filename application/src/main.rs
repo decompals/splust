27215e6bde7e452bd1e2cbdec5380caed399c222
Ethan <ethteck@gmail.com>
2024-02-17 05:28:20 +0900

    it runs

--- application/src/main.rs
+++ application/src/main.rs
@@ -1,3 +1,12 @@
+use interface::{load_root_module_in_directory, ExampleSeg_Ref};
+
 fn main() {
-    println!("Hello, world!");
+    // The type annotation is for the reader
+    let segment: ExampleSeg_Ref =
+        load_root_module_in_directory("target/debug".as_ref()).unwrap_or_else(|e| panic!("{}", e));
+
+    {
+        let response = segment;
+    }
+    println!("success");
 }
